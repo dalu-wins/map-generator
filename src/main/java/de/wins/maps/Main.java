07c1fdc46d70512717046e0044595a11886709a5
Dalu Wins <60736194+dalu-wins@users.noreply.github.com>
2023-07-01 18:14:43 +0200

    removed useless map variables

--- src/main/java/de/wins/maps/Main.java
+++ src/main/java/de/wins/maps/Main.java
@@ -12,11 +12,11 @@ import de.wins.maps.mapbuilder.MapBuilder;
 public class Main {
     public static void main(String[] args) {
 
-        Map map1 = new MapBuilder(257, 257)
+        new MapBuilder(257, 257)
             .generate().save("maps/default/basic")
             .colorize().save("maps/default/colorized");
 
-        Map map2 = new HeightMapBuilder(257)
+        new HeightMapBuilder(257)
             .generate().save("maps/diamondsquare/basic")
             .colorize().save("maps/diamondsquare/colorized")
             .smooth().save("maps/diamondsquare/smoothed")
